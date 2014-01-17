44be7c1176c144f9e527348079c6f150631db29e
Damian Mullins <damian@lowflyingowls.co.uk>
2014-01-17 14:14:31 +0000

    Added missing product service to solution

---
+++ Coinage.Domain/Abstract/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Coinage.Domain.Concrete.Entities;
+using System.Collections.Generic;
+
+namespace Coinage.Domain.Abstract.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+        Product GetProduct(int id);
+    }
+}
