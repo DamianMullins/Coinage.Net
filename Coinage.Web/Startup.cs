638d52afff26b268a0a888a5fd19032c23da4874
Damian Mullins <damian@lowflyingowls.co.uk>
2014-01-17 11:48:43 +0000

    Initial commit

---
+++ Coinage.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Coinage.Web.Startup))]
+namespace Coinage.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
