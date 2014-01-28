cf10d322bd3c7ce8bd1f0bedaf6ba9e497cb427b
Damian Mullins <damian@lowflyingowls.co.uk>
2014-01-28 15:52:36 +0000

    Added basket route

--- Coinage.Web/App_Start/RouteConfig.cs
+++ Coinage.Web/App_Start/RouteConfig.cs
@@ -17,6 +17,13 @@ namespace Coinage.Web
             );
 
             routes.MapRoute(
+                name: "Basket",
+                url: "basket",
+                defaults: new { controller = "Basket", action = "Index" },
+                namespaces: new[] { "Coinage.Web.Controllers" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
