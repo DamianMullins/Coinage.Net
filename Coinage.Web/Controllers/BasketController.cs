0720a6d3c49860af554b35888d3ce4f00aca1455
Damian Mullins <damian@lowflyingowls.co.uk>
2014-01-28 16:01:11 +0000

    Added basket controller & began implementing

---
+++ Coinage.Web/Controllers/BasketController.cs
@@ -0,0 +1,63 @@
+﻿using Coinage.Domain.Abstract.Services;
+using Coinage.Domain.Concrete;
+using Coinage.Domain.Concrete.Entities;
+using Coinage.Web.Models.Basket;
+using System.Web.Mvc;
+
+namespace Coinage.Web.Controllers
+{
+    public class BasketController : BaseController
+    {
+        private readonly IProductService _productService;
+        private readonly IBasketService _basketService;
+
+        public BasketController(IProductService productService, IBasketService basketService)
+        {
+            _productService = productService;
+            _basketService = basketService;
+        }
+
+        public ActionResult Index()
+        {
+            var model = new BasketModel();
+            Basket basket = _basketService.GetBasket(2);
+
+            if (basket != null)
+            {
+                // TODO: Add basket model properties
+
+                foreach (BasketItem item in basket.BasketItems)
+                {
+                    var basketItem = new BasketModel.BasketItemModel
+                    {
+                        Id = item.ProductId,
+                        ProductId = item.Product.Id,
+                        ItemName = item.Product.Name,
+                        Quantity = item.Quantity,
+                        UnitPrice = "", // String formatted individual price
+                        SubTotal = "" // String formatted price * quantity
+                    };
+                    model.Items.Add(basketItem);
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(int productId, int quantity)
+        {
+            Basket basket = _basketService.GetBasket(2);
+            Product product = _productService.GetProductById(productId);
+            EntityActionResponse response = _basketService.AddToCart(basket, product, quantity);
+
+            if (response.Successful)
+            {
+                SuccessAlert(string.Format("{0} was added to your basket", product.Name));
+            }
+            else
+            {
+                ErrorAlert("was added to your basket");
+            }
+            return RedirectToRoute("Basket");
+        }
+	}
+}
---
+++ Coinage.Web/Models/Basket/BasketModel.cs
@@ -0,0 +1,24 @@
+﻿using Coinage.Web.Framework.Mvc;
+using System.Collections.Generic;
+
+namespace Coinage.Web.Models.Basket
+{
+    public class BasketModel
+    {
+        public IList<BasketItemModel> Items { get; set; }
+
+        public BasketModel()
+        {
+            Items = new List<BasketItemModel>();
+        }
+
+        public class BasketItemModel : EntityModel
+        {
+            public int ProductId { get; set; }
+            public int Quantity { get; set; }
+            public string ItemName { get; set; }
+            public string UnitPrice { get; set; }
+            public string SubTotal { get; set; }
+        }
+    }
+}
