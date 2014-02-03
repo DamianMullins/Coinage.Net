33968d7cc75cd0f92ccbe4764f089fd001d50ee3
Damian Mullins <damian@lowflyingowls.co.uk>
2014-02-03 15:38:00 +0000

    Added missing class files. Oops

---
+++ Coinage.Domain/Concrete/Services/CustomerService.cs
@@ -0,0 +1,107 @@
+﻿using System.Linq;
+using Coinage.Domain.Abstract.Data;
+using Coinage.Domain.Abstract.Services;
+using Coinage.Domain.Concrete.Entities;
+using System;
+
+namespace Coinage.Domain.Concrete.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IRepository<Customer> _customerRepository;
+
+        public CustomerService(IRepository<Customer> customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return _customerRepository.GetById(id);
+        }
+
+        public Customer GetCustomerByGuid(Guid customerGuid)
+        {
+            if (customerGuid == Guid.Empty) return null;
+
+            Customer customer = _customerRepository.Table
+                .Where(c => c.CustomerGuid == customerGuid)
+                .OrderByDescending(c => c.CreatedOn)
+                .FirstOrDefault();
+
+            return customer;
+        }
+
+        public Customer GetCustomerByEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email)) return null;
+
+            Customer customer = _customerRepository.Table
+                .Where(c => c.Email == email)
+                .OrderByDescending(c => c.CreatedOn)
+                .FirstOrDefault();
+
+            return customer;
+        }
+
+        public Customer InsertGuestCustomer()
+        {
+            var customer = new Customer
+            {
+                Active = true
+            };
+
+            // TODO: Implement Customer roles & retrieve guest role
+            //customer.Roles.Add
+
+            EntityActionResponse response = Create(customer);
+            return response.Successful ? customer : null;
+        }
+
+        public EntityActionResponse Update(Customer customer)
+        {
+            var response = new EntityActionResponse();
+            if (customer != null)
+            {
+                try
+                {
+                    customer.ModifiedOn = DateTime.Now;
+                    _customerRepository.Update(customer);
+                    response.Successful = true;
+                }
+                catch (Exception ex)
+                {
+                    response.Exception = ex;
+                }
+            }
+            else
+            {
+                response.Exception = new ArgumentNullException("customer");
+            }
+            return response;
+        }
+
+        public EntityActionResponse Create(Customer customer)
+        {
+            var response = new EntityActionResponse();
+            if (customer != null)
+            {
+                try
+                {
+                    customer.CreatedOn = DateTime.Now;
+                    _customerRepository.Insert(customer);
+                    response.Successful = true;
+                }
+                catch (Exception ex)
+                {
+                    response.Exception = ex;
+                }
+            }
+            else
+            {
+                response.Exception = new ArgumentNullException("customer");
+            }
+            return response;
+        }
+    }
+}
---
+++ Coinage.Web.Framework/WebWorkContext.cs
@@ -0,0 +1,98 @@
+﻿using Coinage.Domain.Abstract;
+using Coinage.Domain.Abstract.Authentication;
+using Coinage.Domain.Abstract.Services;
+using Coinage.Domain.Concrete.Entities;
+using System;
+using System.Web;
+
+namespace Coinage.Web.Framework
+{
+    public class WebWorkContext : IWorkContext
+    {
+        private const string CustomerCookieName = "coinage.customer";
+
+        private readonly ICustomerService _customerService;
+        private readonly IAuthenticationService _authenticationService;
+        private readonly HttpContextBase _httpContext;
+
+        private Customer _cachedCustomer;
+
+        public WebWorkContext(ICustomerService customerService, IAuthenticationService authenticationService, HttpContextBase httpContext)
+        {
+            _customerService = customerService;
+            _authenticationService = authenticationService;
+            _httpContext = httpContext;
+        }
+
+        public Customer CurrentCustomer
+        {
+            get
+            {
+                if (_cachedCustomer != null)
+                {
+                    return _cachedCustomer;
+                }
+
+                Customer customer = _authenticationService.GetAuthenticatedCustomer();
+
+
+                // Load guest Customer
+                if (customer == null || !customer.Active)
+                {
+                    HttpCookie customerCookie = GetCustomerCookie();
+                    if (customerCookie != null && !string.IsNullOrEmpty(customerCookie.Value))
+                    {
+                        Guid customerGuid;
+                        if (Guid.TryParse(customerCookie.Value, out customerGuid))
+                        {
+                            var customerByCookie = _customerService.GetCustomerByGuid(customerGuid);
+                            if (customerByCookie != null) // TODO: Implement customer extension methods && !customerByCookie.IsRegistered())
+                            {
+                                customer = customerByCookie;
+                            }
+                        }
+                    }
+                }
+
+                // Create new guest Customer
+                if (customer == null || !customer.Active)
+                {
+                    customer = _customerService.InsertGuestCustomer();
+                }
+
+                if (customer.Active)
+                {
+                    SetCustomerCookie(customer.CustomerGuid);
+                    _cachedCustomer = customer;
+                }
+
+                return _cachedCustomer;
+            }
+            set
+            {
+                SetCustomerCookie(value.CustomerGuid);
+                _cachedCustomer = value;
+            }
+        }
+
+        private HttpCookie GetCustomerCookie()
+        {
+            return _httpContext == null ? null : _httpContext.Request.Cookies[CustomerCookieName];
+        }
+
+        private void SetCustomerCookie(Guid customerGuid)
+        {
+            if (_httpContext == null) return;
+
+            var cookie = new HttpCookie(CustomerCookieName)
+            {
+                HttpOnly = true,
+                Value = customerGuid.ToString(), 
+                Expires = customerGuid == Guid.Empty ? DateTime.Now.AddMonths(-1) : DateTime.Now.AddYears(1)
+            };
+
+            _httpContext.Response.Cookies.Remove(CustomerCookieName);
+            _httpContext.Response.Cookies.Add(cookie);
+        }
+    }
+}
