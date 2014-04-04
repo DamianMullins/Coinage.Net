05b617e430513e2d9f9d7194e21dc42b1773137d
DamianMullins <damian@lowflyingowls.co.uk>
2014-04-04 17:45:12 +0100

    Fixed product link on basket view

---
+++ Coinage.Web/Models/Products/FeaturedProductsModel.cs
@@ -0,0 +1,15 @@
+﻿using Coinage.Domain.Concrete.Entities;
+using System.Collections.Generic;
+
+namespace Coinage.Web.Models.Products
+{
+    public class FeaturedProductsModel
+    {
+        public List<Product> Products { get; set; }
+
+        public FeaturedProductsModel()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
---
+++ Coinage.Web/Models/Products/LatestProductModel.cs
@@ -0,0 +1,15 @@
+﻿using Coinage.Domain.Concrete.Entities;
+using System.Collections.Generic;
+
+namespace Coinage.Web.Models.Products
+{
+    public class LatestProductModel
+    {
+        public List<Product> Products { get; set; }
+
+        public LatestProductModel()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
