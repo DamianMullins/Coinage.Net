4654c89f9ad6a91977603ec64a149735255358c9
DamianMullins <damian@lowflyingowls.co.uk>
2014-04-23 16:52:21 +0100

    Added untracked file

---
+++ Coinage.Domain/Services/IEditableServiceAsync.cs
@@ -0,0 +1,24 @@
+﻿using Coinage.Domain.Entites;
+using Coinage.Domain.Models;
+using System.Threading.Tasks;
+
+namespace Coinage.Domain.Services
+{
+    public interface IEditableServiceAsync<in T> : IEditableService<T> 
+        where T : EditableEntity
+    {
+        /// <summary>
+        /// Asynchronously update an entity.
+        /// </summary>
+        /// <param name="entity">Entity to update.</param>
+        /// <returns>EntityActionResponse object.</returns>
+        Task<EntityActionResponse> UpdateAsync(T entity);
+
+        /// <summary>
+        /// Asynchronously create an entity.
+        /// </summary>
+        /// <param name="entity">Entity to create.</param>
+        /// <returns>EntityActionResponse object.</returns>
+        Task<EntityActionResponse> CreateAsync(T entity);
+    }
+}
